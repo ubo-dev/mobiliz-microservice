e3a59e734959a0a148acffd0c70801b864a314b5
ubo-dev <umutokur11@outlook.com>
2023-08-13 22:30:14 +0300

    grouping-service initial commit

---
+++ grouping-service/src/test/java/com/ubo/groupingservice/GroupingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubo.groupingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
