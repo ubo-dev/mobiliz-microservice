dd217eda6e09693a9473906fae1de3efdc87dd18
ubo-dev <umutokur11@outlook.com>
2023-08-16 21:32:00 +0300

    api-gateway and eureka initial commit

---
+++ api-gateway/src/test/java/com/ubo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
